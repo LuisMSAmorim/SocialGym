3f8b005e58971accd2040b4beca6a0001293a98c
Luís Miguel Silva Amorim <83029922+LuisMSAmorim@users.noreply.github.com>
2022-06-05 13:22:15 -0300

    Add not found message from profile not found

---
+++ SocialGym.Web/Controllers/ProfileController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SocialGym.BLL.ViewModels;
+
+namespace SocialGym.Web.Controllers;
+
+public class ProfileController : Controller
+{
+
+    private readonly string baseUrl = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("Urls")["ApiUrl"];
+
+    [Route("/profile/{userName}")]
+    public async Task<IActionResult> Index(string userName)
+    {
+        HttpClient httpClient = new();
+
+        HttpResponseMessage response = await httpClient.GetAsync($"{baseUrl}/profiles/{userName}");
+
+        string apiResponse = await response.Content.ReadAsStringAsync();
+
+        var profile = JsonConvert.DeserializeObject<UserProfileViewModel>(apiResponse);
+
+        if(profile.UserName == null)
+        {
+            ViewBag.ErrorMessage = "Usuário não encontrado";
+            return View(null);
+        }
+
+        return View(profile);
+    }
+}
